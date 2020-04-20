5da38137f8c06148c56741147ff65e3beefbcb70
Quentin Lurkin <qlurkin@gmail.com>
2020-04-20 20:23:01 +0200

    Lazy

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
-thefuck	--alias | source
-
 #set PATH $PATH "/home/ql/opt/android-studio/bin"
 
 #set PATH $PATH "/home/ql/opt/flutter/bin"
 
 #set PATH "/home/ql/anaconda3/bin" $PATH
+
+#set PYTHONPATH "$HOME/python-module" $PYTHONPATH
 
 source "/home/ql/anaconda3/etc/fish/conf.d/conda.fish"
 conda activate base
