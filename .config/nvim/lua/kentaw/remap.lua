2f5dac224417c2baa35f239defa988d673752698
Quentin Lurkin <qlurkin@gmail.com>
2023-07-16 22:36:45 +0200

    Add tmux config

--- .config/nvim/lua/kentaw/remap.lua
+++ .config/nvim/lua/kentaw/remap.lua
@@ -2,15 +2,18 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 vim.keymap.set('x', ">", ">gv")
 vim.keymap.set('x', "<", "<gv")
-vim.keymap.set('n', "<leader>wh", "<C-w>h")
-vim.keymap.set('n', "<leader>wl", "<C-w>l")
-vim.keymap.set('n', "<leader>wj", "<C-w>j")
-vim.keymap.set('n', "<leader>wk", "<C-w>k")
+-- vim.keymap.set('n', "<C-h>", "<C-w>h")
+-- vim.keymap.set('n', "<C-l>", "<C-w>l")
+-- vim.keymap.set('n', "<C-j>", "<C-w>j")
+-- vim.keymap.set('n', "<C-k>", "<C-w>k")
 vim.keymap.set('n', "<C-d>", "<C-d>zz")
 vim.keymap.set('n', "<C-u>", "<C-u>zz")
 vim.keymap.set('n', "n", "nzzzv")
 vim.keymap.set('n', "N", "Nzzzv")
 vim.keymap.set('n', "<leader>e", function () vim.cmd('Ex') end)
+vim.keymap.set('n', "<leader>i", "<cmd> vsplit<cr>")
+vim.keymap.set('n', "<leader>-", "<cmd> split<cr>")
+
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  'christoomey/vim-tmux-navigator',
+  lazy = false,
+  config = function ()
+    vim.keymap.set('n', "<C-h>", "<cmd> TmuxNavigateLeft<cr>")
+    vim.keymap.set('n', "<C-l>", "<cmd> TmuxNavigateRight<cr>")
+    vim.keymap.set('n', "<C-j>", "<cmd> TmuxNavigateDown<cr>")
+    vim.keymap.set('n', "<C-k>", "<cmd> TmuxNavigateUp<cr>")
+  end
+}
