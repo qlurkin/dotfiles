4800bfba4f625971b8ac392efdb4fd6c95e3626f
Quentin Lurkin <qlurkin@gmail.com>
2023-09-21 09:44:01 +0200

    Do not show hidden files in Telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function ()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', "<leader><leader>", function () builtin.find_files({hidden = true, no_ignore = true}) end, {desc = "Find Files"})
+    vim.keymap.set('n', "<leader><leader>", function () builtin.find_files({no_ignore = true}) end, {desc = "Find Files"})
     vim.keymap.set('n', "<leader>ff", builtin.find_files, {desc = "Find Files"})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Grep"})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Buffers"})
