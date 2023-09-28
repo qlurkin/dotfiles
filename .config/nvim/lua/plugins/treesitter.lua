92bdb444597a075d218fb45eeab839235c122e05
Quentin Lurkin <qlurkin@gmail.com>
2023-09-28 22:01:07 +0200

    Change Incremental Selection Keymap

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   keys = {
-    { "<c-i>", desc = "Increment selection" },
+    { "<c-space>", desc = "Increment selection" }, -- MacOS use this shorcut. Disable it !
     { "<bs>", desc = "Decrement selection", mode = "x" },
   },
   opts = {
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "<C-i>",
-        node_incremental = "<C-i>",
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
         scope_incremental = false,
         node_decremental = "<bs>",
       },
