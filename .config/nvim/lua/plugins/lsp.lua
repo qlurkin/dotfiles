36ded437fe8a94b7d5b4f1f5533e8eca0d7cdafc
Quentin Lurkin <qlurkin@gmail.com>
2024-06-08 10:28:15 +0200

    Lazy

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,30 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    opts.servers.cssls = {}
-    opts.servers.wgsl_analyzer = {}
-    opts.emmet_language_server = {
-      filetypes = {
-        "eruby",
-        "html",
-        "javascript",
-        "javascriptreact",
-        "svelte",
-        "pug",
-        "typescriptreact",
-        "vue",
-        "markdown",
-      },
-    }
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "wgsl-analyzer")
+      table.insert(opts.ensure_installed, "css-lsp")
+      table.insert(opts.ensure_installed, "emmet-language-server")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers.cssls = {}
+      opts.servers.wgsl_analyzer = {}
+      opts.emmet_language_server = {
+        filetypes = {
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "svelte",
+          "pug",
+          "typescriptreact",
+          "vue",
+          "markdown",
+        },
+      }
+    end,
+  },
 }
--- .config/nvim/lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    table.insert(opts.ensure_installed, "wgsl-analyzer")
-    table.insert(opts.ensure_installed, "css-lsp")
-  end,
-}
