45699ded080c1394dc57e37ef722c18b9060ae09
Quentin Lurkin <qlurkin@gmail.com>
2025-07-05 16:43:08 +0200

    Updates

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -21,19 +21,19 @@ return {
  [Powered by LazyVim]
   ]],
       },
-      -- sections = {
-      --   {
-      --     section = "terminal",
-      --     cmd = "dbget -w 50 --height 30 --author",
-      --     random = 10,
-      --     ttl = 60,
-      --     indent = 0,
-      --     height = 30,
-      --   },
-      --   { section = "header", pane = 2 },
-      --   { section = "keys", pane = 2, gap = 1, padding = 1 },
-      --   { section = "startup", pane = 2 },
-      -- },
+      sections = {
+        {
+          section = "terminal",
+          cmd = "dbget -w 50 --height 30 --author",
+          random = 10,
+          ttl = 60,
+          indent = 0,
+          height = 30,
+        },
+        { section = "header", pane = 2 },
+        { section = "keys", pane = 2, gap = 1, padding = 1 },
+        { section = "startup", pane = 2 },
+      },
     },
   },
 }
