97db2eb3ab356de0f2f5d6a3d8f2095a4b0b506d
Quentin Lurkin <qlurkin@gmail.com>
2026-03-04 10:23:57 +0100

    Fix tabs BG

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -68,7 +68,9 @@ local theme = wezterm.color.get_builtin_schemes()[config.color_scheme]
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
 	local edge_background = theme.tab_bar.background
-	local background = theme.selection_bg
+
+	-- inactive tabs
+	local background = wezterm.color.parse(edge_background):lighten(0.15)
 	local foreground = theme.tab_bar.inactive_tab.fg_color
 	local index_bg = theme.tab_bar.inactive_tab_hover.bg_color
 	local index_fg = theme.tab_bar.inactive_tab_hover.fg_color
